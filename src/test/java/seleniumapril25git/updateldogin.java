9ce60b85d17e1c95928b763ea55eae53809b3638
ash1995p <ashwinipudale2@gmail.com>
2025-07-09 17:19:14 +0530

    updatelogin file commited

---
+++ src/test/java/seleniumapril25git/updateldogin.java
@@ -0,0 +1,26 @@
+package seleniumapril25git;
+
+public class updateldogin {
+
+	  
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = new String ("6666");
+		StringBuffer s2 = new StringBuffer(s1);
+		String s3 = new String (s2.reverse());
+		System.out.println(s3);
+		
+		if(s3.equals(s1))
+		{
+			System.out.println("palindrom");
+		}
+		else
+		{
+			System.out.println("not palindrom");
+		}
+
+	}
+
+}
